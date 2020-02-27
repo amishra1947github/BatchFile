5d10e185fe775bb7fe69bc0ff31043e51140d7c9
amishra1947github <56932581+amishra1947github@users.noreply.github.com>
2020-02-27 08:37:15 +0530

    Add files via upload

---
+++ LCOportalSSH.bat
@@ -0,0 +1,4 @@
+start putty 172.21.60.73 -l mobiotics -pw Mobi@2019
+start putty 172.21.60.74 -l mobiotics -pw Mobi@2019
+start putty 172.21.60.93 -l mobiotics -pw Mobi@2019
+start putty 172.21.60.94 -l mobiotics -pw Mobi@2019
